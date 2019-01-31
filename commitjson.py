aa14b0901f06da8f867663901a8962fd007d1d42
Jacob Elstein <jacobelstein@gmail.com>
2019-01-30 16:17:05 -0800

    setup meetingdatatest submodule

--- commitjson.py
+++ commitjson.py
@@ -2,7 +2,6 @@ import git
 import datetime
 
 def commitjson():
-	repo = git.Repo('./meetingdata')
+	repo = git.Repo('./meetingdata-test')
 	repo.git.add('meetingdata.json')
 	repo.git.commit('-m',datetime.datetime.now())
-	repo.git.push('origin','master')
--- main.py
+++ main.py
@@ -6,13 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-	file = open("meetingdata/meetingdata.json", "w")
+	file = open("meetingdatatest/meetingdata.json", "w")
 	file.write(datatojson())
 	file.close()
 	#commitjson()
 
-	#heroku test of file writing
-	with open("meetingdata/meetingdata.json", "rb") as myfile:
-		data_to_read = myfile.read()
-
-	return data_to_read
+	return "Success"
