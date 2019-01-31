561b5f686aeeb0a7e4979c7d28b007c9d93996d8
Jacob Elstein <jacobelstein@gmail.com>
2019-01-31 10:51:19 -0800

    Back to a pure JSON feed app, no git actions

--- commitjson.py
+++
@@ -1,8 +0,0 @@
-import git
-import datetime
-
-def commitjson():
-	repo = git.Repo('./meetingdata')
-	repo.git.add('meetingdata.json')
-	repo.git.commit('-m',datetime.datetime.now())
-	repo.git.push('origin','master')
--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 from flask import Flask
 from datatojson import datatojson
-from commitjson import commitjson
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-	file = open("meetingdata/meetingdata.json", "w")
-	file.write(datatojson())
-	file.close()
-	#commitjson()
-
-	#heroku test of file writing
-	with open("meetingdata/meetingdata.json", "rb") as myfile:
-		data_to_read = myfile.read()
-
-	return data_to_read
+	return datatojson()
